22e5533e409803ece58043f8d7c7527f90ef5c98
Mehdi Alikhani <mahdi.alikhani855@hotmail.com>
2023-05-09 13:57:37 +0330

    Refactor Consumer command and accept optional driver parameter

---
+++ src/Console/Commands/ConsumerCommand.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Celysium\MessageBroker\Console\Commands;
+
+use Celysium\MessageBroker\Facades\MessageBroker;
+use Illuminate\Console\Command;
+
+class ConsumerCommand extends Command
+{
+    protected $signature = 'message-broker:consume {--driver=}';
+
+    protected $description = 'Create message broker consumer';
+
+    public function handle(): void
+    {
+        $driver = $this->option('driver') ?? config('MESSAGE_BROKER_DEFAULT_DRIVER');
+
+        MessageBroker::driver($driver)->consumer();
+    }
+}
