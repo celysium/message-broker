7e7650e6b9964d8beb7dc07de5d054cde37d0b20
abbas <abbasmomeny1994@gmail.com>
2023-06-07 14:14:37 +0330

    ref: change __constructor()

--- src/Events/SendMessageEvent.php
+++ src/Events/SendMessageEvent.php
@@ -9,14 +9,7 @@ class SendMessageEvent
 {
     use Dispatchable, SerializesModels;
 
-    public mixed $data;
-    public string $event;
-    public string $service;
-
-    public function __construct($event, $data, $service = 'none')
+    public function __construct(public $event, public array $data, public ?string $driver = null)
     {
-        $this->event = $event;
-        $this->service = $service;
-        $this->data = $data;
     }
 }
