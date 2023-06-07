6cfe37e468dc717f683a8f37056de95334de2048
abbas <abbasmomeny1994@gmail.com>
2023-06-07 14:19:08 +0330

    feat: add binding to MessageBrokerServiceProvider

--- src/MessageBrokerServiceProvider.php
+++ src/MessageBrokerServiceProvider.php
@@ -3,16 +3,22 @@
 namespace Celysium\MessageBroker;
 
 use Celysium\MessageBroker\Console\Commands\ListenerCommand;
+use Celysium\MessageBroker\Events\SendMessageEvent;
+use Celysium\MessageBroker\Listeners\SendMessageListener;
+use Illuminate\Support\Facades\Event;
 use Illuminate\Support\ServiceProvider;
+
 class MessageBrokerServiceProvider extends ServiceProvider
 {
     public function register()
     {
-        $this->app->bind('message-broker', function($app) {
+        Event::listen(SendMessageEvent::class, SendMessageListener::class);
+
+        $this->app->bind('message-broker', function ($app) {
             return new MessageBroker($app);
         });
 
-        $this->mergeConfigFrom( __DIR__ . '/../config/message-broker.php', 'message-broker');
+        $this->mergeConfigFrom(__DIR__ . '/../config/message-broker.php', 'message-broker');
 
         $this->publishes([
             __DIR__ . '/../config/message-broker.php' => config_path('message-broker.php'),
