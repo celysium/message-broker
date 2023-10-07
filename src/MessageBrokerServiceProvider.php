44a34a990e7eb1d9308ed1908cea39ebf527f408
Mehdi Alikhani <mahdi.alikhani855@hotmail.com>
2023-10-07 12:03:30 +0330

    bind queue to exchange when booting

--- src/MessageBrokerServiceProvider.php
+++ src/MessageBrokerServiceProvider.php
@@ -7,6 +7,7 @@ use Celysium\MessageBroker\Events\SendMessageEvent;
 use Celysium\MessageBroker\Listeners\SendMessageListener;
 use Illuminate\Support\Facades\Event;
 use Illuminate\Support\ServiceProvider;
+use PhpAmqpLib\Connection\AMQPStreamConnection;
 
 class MessageBrokerServiceProvider extends ServiceProvider
 {
@@ -25,5 +26,25 @@ class MessageBrokerServiceProvider extends ServiceProvider
         ], 'config');
 
         $this->commands(ListenerCommand::class);
+    }
+
+    public function boot()
+    {
+        $queue = config('message-broker.rabbitmq.queue');
+        $host = config('message-broker.rabbitmq.host');
+        $port = config('message-broker.rabbitmq.port');
+        $user = config('message-broker.rabbitmq.user');
+        $password = config('message-broker.rabbitmq.password');
+        $exchange = config('message-broker.rabbitmq.exchange');
+        $key = config('message-broker.rabbitmq.exchange_key');
+
+        $connection = new AMQPStreamConnection($host, $port, $user, $password);
+
+        $channel = $connection->channel();
+        $channel->exchange_declare($exchange, 'fanout', false, true, false);
+        $channel->queue_declare($queue, false, true, false, false);
+        $channel->queue_bind($queue, $exchange, $key);
+        $channel->close();
+        $connection->close();
     }
 }
